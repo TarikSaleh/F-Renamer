0fc4d2ccdf5e04b610877512cbc5412dd97623f2
Tarek Saleh <Tarekalsaleh86@hotmail.com>
2018-03-03 23:00:51 +0300

    Add project files.

---
+++ Test2/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Test2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TextBox1 = new System.Windows.Forms.TextBox();
+            this.TextBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.Label3 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // TextBox1
+            // 
+            this.TextBox1.Location = new System.Drawing.Point(13, 13);
+            this.TextBox1.Multiline = true;
+            this.TextBox1.Name = "TextBox1";
+            this.TextBox1.Size = new System.Drawing.Size(621, 37);
+            this.TextBox1.TabIndex = 0;
+            // 
+            // TextBox2
+            // 
+            this.TextBox2.Location = new System.Drawing.Point(13, 66);
+            this.TextBox2.Multiline = true;
+            this.TextBox2.Name = "TextBox2";
+            this.TextBox2.Size = new System.Drawing.Size(621, 40);
+            this.TextBox2.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(171, 132);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 36);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "change";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Label3
+            // 
+            this.Label3.AutoSize = true;
+            this.Label3.Location = new System.Drawing.Point(13, 181);
+            this.Label3.Name = "Label3";
+            this.Label3.Size = new System.Drawing.Size(48, 17);
+            this.Label3.TabIndex = 3;
+            this.Label3.Text = "Status";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(347, 132);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 37);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Rename";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(657, 253);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.TextBox2);
+            this.Controls.Add(this.TextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TextBox1;
+        private System.Windows.Forms.TextBox TextBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label Label3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Test2/Form1.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Test2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string strsorc = TextBox1.Text;
+
+            char[] array1 = new char[] { 'Ç', 'ç', 'Ö', 'ö', 'İ', 'ı', ' '};
+            char[] array2 = new char[] { 'C', 'c', 'O', 'o', 'I', 'i', '-'};
+
+            char[] charArrsorc = strsorc.ToArray();
+
+            for (int ch = 0; ch < charArrsorc.Length; ch++)
+            {
+                for (int i = 0; i < array1.Length; i++)
+                {
+                    for (int j = 0; j < array2.Length; j++)
+                    {
+                        if (ch == i)
+                        {
+                            TextBox2.Text = TextBox1.Text.Replace(charArrsorc[ch], array2[j]);
+                        } 
+                    }
+                }
+            }
+
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string strsorc = TextBox1.Text;
+            string strdest = TextBox2.Text;
+            try
+            {
+                FileInfo sourcefile = new FileInfo(strsorc);
+                if (sourcefile.Exists)
+                {
+                    sourcefile.MoveTo(strdest);
+                    Label3.Text = "File Renamed";
+                }
+                else
+                {
+                    Label3.Text = "File Not Found";
+                }
+            }
+            catch (Exception ex)
+            {
+                Label3.Text = ex.Message;
+            }
+        }
+    }
+}
